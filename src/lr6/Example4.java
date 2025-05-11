2ad99212293899ab578a072ffa4067a43761f9c2
IvanDukachev <ivandukachev2002@gmail.com>
2025-05-11 12:19:53 +0500

    lr6

---
+++ src/lr6/Example4.java
@@ -0,0 +1,14 @@
+package lr6;
+
+public class Example4 {
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 1; i < 11; i++) {
+            final int n = i;
+            Thread thread = new Thread(() -> {
+                System.out.printf("Поток №%d\n", n);
+            });
+            thread.start();
+            thread.join();
+        }
+    }
+}
